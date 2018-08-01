697e26d093e3865eb38deb2f83918b7fc0741cde
Felix Neutatz <neutatz@googlemail.com>
2018-08-01 16:24:05 +0200

    generalize output path

--- model/ml/experiments/dBoost_experiment.py
+++ model/ml/experiments/dBoost_experiment.py
@@ -9,11 +9,18 @@ from ml.tools.dboost.TestDBoost import test_multiple_sizes_hist
 from ml.tools.dboost.TestDBoost import test_multiple_sizes_gaussian
 from ml.tools.dboost.TestDBoost import test_multiple_sizes_mixture
 
-
+from ml.configuration.Config import Config
 import numpy as np
+import os
 
 
-data_list =[FlightHoloClean, BlackOakDataSetUppercase, HospitalHoloClean, Restaurant, Movies]
+path_folder = Config.get("logging.folder") + "/out/dboost"
+
+if not os.path.exists(path_folder):
+    os.makedirs(path_folder)
+
+
+data_list = [FlightHoloClean, BlackOakDataSetUppercase, HospitalHoloClean, Restaurant, Movies]
 
 steps = 100 #size of grid
 N = 1
@@ -26,7 +33,7 @@ for dataset in data_list:
     number_rows = data.shape[0] #how big is the evaluation set
     for dBoost in dBoost_methods:
         ts = time.time()
-        log_file = "out/dboost/" + str(data.name) + "_time_" + str(ts) + "_dBoost_" + dBoost.func_name  + ".txt"
+        log_file = path_folder + "/" + str(data.name) + "_time_" + str(ts) + "_dBoost_" + dBoost.func_name  + ".txt"
         dBoost(data, steps, N, [number_rows], log_file)
 
 '''
