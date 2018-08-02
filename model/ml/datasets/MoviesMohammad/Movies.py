4102505b2bc0a80e0c56bddd457734b965f1edad
Felix Neutatz <neutatz@googlemail.com>
2018-08-02 10:08:10 +0200

    generalize paths

--- model/ml/datasets/MoviesMohammad/Movies.py
+++ model/ml/datasets/MoviesMohammad/Movies.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from ml.configuration.Config import Config
 
 from ml.datasets.DataSet import DataSet
 
@@ -7,8 +8,8 @@ from ml.datasets.DataSet import DataSet
 class Movies(DataSet):
 
     def __init__(self):
-        clean_df = pd.read_csv('/home/felix/Software/data-pool/movies/rotten_tomatoes.csv', header=0, dtype=object, na_filter=False)
-        dirty_df = pd.read_csv('/home/felix/Software/data-pool/movies/dirty.csv', header=0, dtype=object, na_filter=False)
+        clean_df = pd.read_csv(Config.get("datapool.folder") + '/movies/rotten_tomatoes.csv', header=0, dtype=object, na_filter=False)
+        dirty_df = pd.read_csv(Config.get("datapool.folder") + '/movies/dirty.csv', header=0, dtype=object, na_filter=False)
 
         super(Movies, self).__init__("Movies", dirty_df, clean_df)
 
--- model/ml/datasets/RestaurantMohammad/Restaurant.py
+++ model/ml/datasets/RestaurantMohammad/Restaurant.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+from ml.configuration.Config import Config
 
 from ml.datasets.DataSet import DataSet
 
@@ -7,8 +8,8 @@ from ml.datasets.DataSet import DataSet
 class Restaurant(DataSet):
 
     def __init__(self):
-        clean_df = pd.read_csv('/home/felix/Software/data-pool/restaurants/yellow_pages.csv', header=0, dtype=object, na_filter=False)
-        dirty_df = pd.read_csv('/home/felix/Software/data-pool/restaurants/dirty.csv', header=0, dtype=object, na_filter=False)
+        clean_df = pd.read_csv(Config.get("datapool.folder") + '/restaurants/yellow_pages.csv', header=0, dtype=object, na_filter=False)
+        dirty_df = pd.read_csv(Config.get("datapool.folder") + '/restaurants/dirty.csv', header=0, dtype=object, na_filter=False)
 
         super(Restaurant, self).__init__("Restaurant", dirty_df, clean_df)
 
