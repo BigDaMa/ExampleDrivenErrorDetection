851efef2cebb5a1273804b29f5d994de96a4b42c
Felix Neutatz <neutatz@googlemail.com>
2018-08-30 11:57:14 +0200

    change beers location

--- model/ml/datasets/BeersMohammad/Beers.py
+++ model/ml/datasets/BeersMohammad/Beers.py
@@ -8,8 +8,8 @@ from ml.datasets.DataSet import DataSet
 class Beers(DataSet):
 
     def __init__(self):
-        clean_df = pd.read_csv('/home/felix/data_more/Beers_Mohammad/clean.csv', header=0, dtype=object, na_filter=False)
-        dirty_df = pd.read_csv('/home/felix/data_more/Beers_Mohammad/dirty.csv', header=0, dtype=object, na_filter=False)
+        clean_df = pd.read_csv(Config.get("datapool.folder") + '/Beers_Mohammad/clean.csv', header=0, dtype=object, na_filter=False)
+        dirty_df = pd.read_csv(Config.get("datapool.folder") + '/Beers_Mohammad/dirty.csv', header=0, dtype=object, na_filter=False)
 
         super(Beers, self).__init__("Beers", dirty_df, clean_df)
 
