66e0337fa40a7e1c3b71fb3e4f77bd9ddf5ce250
Felix Neutatz <neutatz@googlemail.com>
2019-05-03 08:47:28 +0200

    step size =100

--- model/ml/experiments/features_experiment_multi.py
+++ model/ml/experiments/features_experiment_multi.py
@@ -97,8 +97,8 @@ for dataset in data_list:
             my_dict['dataSet'] = data
             my_dict['classifier_model'] = classifier
             my_dict['checkN'] = 1
-            my_dict['label_iterations'] = 15
-            my_dict['step_size'] = 10
+            my_dict['label_iterations'] = 10
+            my_dict['step_size'] = 100
             fnames.append(feature_names[param_i])
 
             my_array.append(my_dict)
