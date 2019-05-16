c97420699f97a9c887578084c5477d4c07950e3e
Felix Neutatz <neutatz@googlemail.com>
2019-05-16 21:15:33 +0200

    hosp50

--- model/ml/experiments/one_classifier_multi.py
+++ model/ml/experiments/one_classifier_multi.py
@@ -31,13 +31,13 @@ from ml.datasets.hospital.HospitalMoreCol import HospitalMoreCol
 
 
 
-path_folder = Config.get("logging.folder") + "/out/one_classifier_Soccer"
+path_folder = Config.get("logging.folder") + "/out/one_classifier_Hosp"
 if not os.path.exists(path_folder):
     os.makedirs(path_folder)
 
 
 #data_list = [FlightHoloClean, BlackOakDataSetUppercase, HospitalHoloClean, Movies, Restaurant, Citation, Beers, Salary]
-data_list = [Soccer]
+data_list = [HospitalMoreCol]
 
 
 parameters = []
