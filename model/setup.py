92743a5ff66f02d779c39111c4fd3d5ebd3dbedf
Felix Neutatz <neutatz@googlemail.com>
2019-05-19 22:47:41 +0200

    fix setup

--- model/setup.py
+++ model/setup.py
@@ -20,7 +20,7 @@ setup(
     license=license,
     package_data={'config': ['ml/configuration/resources']},
     include_package_data=True,
-    install_requires=['matplotlib', 'numpy', 'pandas', 'sklearn', 'xgboost', 'seaborn', 'graphviz', 'jinja2', 'scipy', 'eli5', 'psycopg2', 'keras', 'tensorflow', 'usaddress'],
+    install_requires=['matplotlib', 'numpy', 'pandas', 'sklearn', 'xgboost', 'seaborn', 'graphviz', 'jinja2', 'scipy', 'eli5', 'psycopg2', 'keras', 'tensorflow', 'usaddress', 'gensim'],
     packages=find_packages(exclude=('tests', 'docs'))
 )
 
