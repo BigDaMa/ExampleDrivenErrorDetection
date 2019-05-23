8555bf64a5f792603db8c0faca0ac7bbe607323a
Felix Neutatz <neutatz@googlemail.com>
2019-05-23 09:00:27 +0200

    run soccer only

--- model/ml/experiments/one_classifier_multi.py
+++ model/ml/experiments/one_classifier_multi.py
@@ -37,7 +37,7 @@ if not os.path.exists(path_folder):
 
 
 #data_list = [FlightHoloClean, BlackOakDataSetUppercase, HospitalHoloClean, Movies, Restaurant, Citation, Beers, Salary]
-data_list = [Soccer, HospitalMoreCol, Adult]
+data_list = [Soccer]
 
 
 parameters = []
@@ -100,7 +100,7 @@ for dataset in data_list:
             my_dict['dataSet'] = data
             my_dict['classifier_model'] = classifier
             my_dict['checkN'] = 1
-            my_dict['label_iterations'] = 40 #adults
+            my_dict['label_iterations'] = 9#40 #adults
             my_dict['step_size'] = 50
             fnames.append(feature_names[param_i])
 
